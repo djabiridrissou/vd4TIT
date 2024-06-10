2ce844f1ca5305d83ac9bd0f4c72dd34d5f9772d
djabiridrissou <djabirtairou@gmail.com>
2024-06-10 05:09:11 +0000

    fix: routes not working on vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import './index.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
